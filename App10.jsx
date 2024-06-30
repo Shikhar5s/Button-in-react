93769729f56b3e4cb59f35bd74589426bbc58687
Shikhar Srivastava <144626521+Shikhar5s@users.noreply.github.com>
2024-07-01 02:00:30 +0530

    Add files via upload

---
+++ App10.jsx
@@ -0,0 +1,67 @@
+// it renders todo on clicking buttons
+
+import {useState} from "react"
+
+import { useEffect } from "react"
+import axios from "axios"
+
+
+function App10(){
+     const[selectedId,setSelectedId]=useState(1)
+
+
+     
+
+    return <div>
+        <button onClick={function(){
+            setSelectedId(1)
+        }}>1</button>
+        <button onClick={function(){
+            setSelectedId(2)
+        }}>2</button>
+        <button  onClick={function(){
+            setSelectedId(3)}}>3</button>
+        <button  onClick={function(){
+            setSelectedId(4)}}>4</button>
+        <Todo id={selectedId}></Todo>
+    </div>
+
+}
+
+function Todo({id}){
+ 
+
+    const[todo,setTodo]=useState({});
+
+
+    useEffect(()=>{
+        axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      
+
+        .then(response=>{
+
+            setTodo(response.data.todo)
+        })
+
+
+    
+    },[id])
+
+
+
+
+
+return <div>
+   Id:{id}
+
+    <h1>
+        {todo.title}
+    </h1>
+    <h4>
+        {todo.description}
+    </h4>
+</div>
+
+}
+
+export default App10;
---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App10 from './App10.jsx'
+//import App2 from './App2.jsx'
+//import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <App10/>
+  
+  </React.StrictMode>,
+)
